08493b24c38636fd927b9f9dcbc0d6af4bc109ef
Sumayya Usman <sumayyamuhammadusman@gmail.com>
2022-12-08 13:40:26 -0800

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,23 +3,12 @@
 
 
 /**
- *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (success)
- *
- *     */
+ *main - Entry point
+ *Return: Always 0 (success)
+ */
 
 int main(void)
-
 {
-
-		printf("with proper grammar, but the outcome is a piece of art,\n");
-
-			return (0);
-
-
-
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
