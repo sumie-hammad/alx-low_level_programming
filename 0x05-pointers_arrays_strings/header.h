636779f438413c3bfa27c20eacfb4514a0791956
Sumayya Usman <sumayyamuhammadusman@gmail.com>
2022-12-20 19:27:27 -0800

    Task 3,4

--- 0x05-pointers_arrays_strings/header.h
+++ 0x05-pointers_arrays_strings/header.h
@@ -1,9 +1,11 @@
 #ifndef HEADER_H
 #define HEADER_H
 
+int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
+
 #endif
