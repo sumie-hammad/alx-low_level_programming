87e73650bc29a6f4aecd2afeb4d3c17c88e0fd6a
Sumayya Usman <sumayyamuhammadusman@gmail.com>
2022-12-20 16:09:36 -0800

    Task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -7,12 +7,18 @@
  */
 
 int main(void)
-
-{     
- 	int i;
-        
-	i = 0;
-       	positive_or_negative(i);
-	
-       	return (0);
+{
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else
+	{
+		printf("%d is negative\n", n);
+	}
+	return (0);
 }
