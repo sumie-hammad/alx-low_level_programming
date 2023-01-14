64cdc127c375bed556801bc594711b6a4ba63a6e
Sumayya Usman <sumayyamuhammadusman@gmail.com>
2023-01-14 12:20:34 -0800

    Task 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,7 +1,6 @@
 #include "main.h"
 /**
- * print_numbers function that prints the numbers, from 0 to 9
- *
+ * print_numbers - function that prints the numbers, from 0 to 9
  * Return: 0-9 followed by new line
  */
 void print_numbers(void)
