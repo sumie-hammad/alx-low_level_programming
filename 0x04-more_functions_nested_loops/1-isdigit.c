54bf14da453bbe3798c58507ae3cf24f5117134a
Sumayya Usman <sumayyamuhammadusman@gmail.com>
2023-01-14 11:38:49 -0800

    Task 1

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
- * -isdigit - functions that verifies if a character is a digit or not
+ * _isdigit - functions that verifies if a character is a digit or not
  *  @c: tested character
  *  Return: return 1 if is uppercase , 0 if not
  */
 
-int -isdigit(int c)
+int _isdigit(int c)
 {
 if ((c >= 48) && (c <= 57))
 return (1);
-
+else
 return (0)
 }
