4bb345813265b5b7f2dd29c055257216ee75a009
Sumayya Usman <sumayyamuhammadusman@gmail.com>
2023-01-15 13:48:39 -0800

    Task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,25 +1,20 @@
 #include "main.h"
+
 /**
- * more_numbers - printingmore numbers
- *
- * Return: return nothing
- */
+* more_numbers -> printing more numbers
+*/
 void more_numbers(void)
 {
-int number1, number2;
+	int x, y;
 
-for (number1 = 0; number1 < 10; number++)
-{
-for (number2 = 0; number2 < 15; number2++)
-
-i{
-if (number2 > 9)
-{
-_putchar((number2 / 10) + '0');
+	for (x = 0; x < 10; x++)
+	{
+		for (y = 0; y < 15; y++)
+		{
+			if (y >= 10)
+				_putchar((y / 10) + '0');
+			_putchar((y % 10) + '0');
+		}
+		_putchar('\n');
+	}
 }
-_putchar((number2 % 10) + '0');
-}
-_putchar(10);
-}
-}
-
