b0c10854ad3094cb2758a6f0c76b5b96fd7fffb4
Sumayya Usman <sumayyamuhammadusman@gmail.com>
2023-01-16 12:03:28 -0800

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,20 @@
 #include "main.h"
 
 /**
- * main - Entry point
- *
- * Print: characters in a string
- *
- * Return: Always 0 (Success)
- */
+* main - prints putchar
+* Return: always 0
+*/
 int main(void)
 {
-	char str[] = "_putchar";
-	int i;
-
-	for (i = 0; str[i] != '\0'; i++)
-		_putchar(str[i]);
-
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
+
 	return (0);
 }
